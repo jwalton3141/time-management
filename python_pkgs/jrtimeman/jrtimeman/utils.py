71d3cb0d672a33fa1961d86fa31df5505ddf4e60
Jamie <jamie@jumpingrivers.com>
2021-09-16 06:58:18 +0100

    lint

--- python_pkgs/jrtimeman/jrtimeman/utils.py
+++ python_pkgs/jrtimeman/jrtimeman/utils.py
@@ -1,15 +1,15 @@
 def dict_keys_to_lower(x: dict) -> dict:
-  """
-  Transform dictionary keys to all lower case.
-  Primary use case is for taking the names of env varibles that 
-  are typically upper case in a dictionary and passing them 
-  as function arguments which are typically lower case kwargs.
+    """
+    Transform dictionary keys to all lower case.
+    Primary use case is for taking the names of env varibles that
+    are typically upper case in a dictionary and passing them
+    as function arguments which are typically lower case kwargs.
 
-  Parameters
-  ----------
-  x : dict 
-    a dictionary
-  """
-  return {
-    k.lower() : v for k, v in x.items()
-  }
+    Parameters
+    ----------
+    x : dict
+      a dictionary
+    """
+    return {
+        k.lower(): v for k, v in x.items()
+    }
