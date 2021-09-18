0a12e7889f6b2f7d947a9371ace1f84b9c6c7c8c
Jack Walton <jwalton3141@gmail.com>
2021-09-18 16:28:17 +0100

    minor docstring changes

--- python_pkgs/jrtimeman/jrtimeman/utils.py
+++ python_pkgs/jrtimeman/jrtimeman/utils.py
@@ -5,6 +5,7 @@ from typing import Tuple
 def dict_keys_to_lower(x: dict) -> dict:
     """
     Transform dictionary keys to all lower case.
+
     Primary use case is for taking the names of env varibles that
     are typically upper case in a dictionary and passing them
     as function arguments which are typically lower case kwargs.
@@ -12,7 +13,12 @@ def dict_keys_to_lower(x: dict) -> dict:
     Parameters
     ----------
     x : dict
-      a dictionary
+        Dictionary to lowercase keys of.
+
+    Returns
+    ------
+    dict
+        Dictionary with same values as input, but lowercased keys.
     """
     return {k.lower(): v for k, v in x.items()}
 
