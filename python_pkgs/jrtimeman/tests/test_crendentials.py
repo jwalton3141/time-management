e3bb3ff2e100855dfe36fcf9785b72e7f3146dc9
Jack Walton <jwalton3141@gmail.com>
2021-09-21 22:11:48 +0100

    fix tests

--- python_pkgs/jrtimeman/tests/test_crendentials.py
+++ python_pkgs/jrtimeman/tests/test_crendentials.py
@@ -10,12 +10,12 @@ class TestCredentialsHelpers(unittest.TestCase):
         # test correct failure and pass on single and multiple inputs
         os.environ["COLIN"] = "slavedriver"
         os.environ["ESTHER"] = "mrs slavedriver"
-        self.assertTrue(jrtimeman.credentials.has_env_vars())
-        self.assertTrue(jrtimeman.credentials.has_env_vars("COLIN"))
-        self.assertTrue(jrtimeman.credentials.has_env_vars("COLIN", "ESTHER"))
-        self.assertFalse(jrtimeman.credentials.has_env_vars("JAMIE"))
-        self.assertFalse(jrtimeman.credentials.has_env_vars("JAMIE", "JACK"))
-        self.assertFalse(jrtimeman.credentials.has_env_vars("JAMIE", "COLIN"))
+        self.assertTrue(jrtimeman.credentials.has_env_vars([]))
+        self.assertTrue(jrtimeman.credentials.has_env_vars(["COLIN"]))
+        self.assertTrue(jrtimeman.credentials.has_env_vars(["COLIN", "ESTHER"]))
+        self.assertFalse(jrtimeman.credentials.has_env_vars(["JAMIE"]))
+        self.assertFalse(jrtimeman.credentials.has_env_vars(["JAMIE", "JACK"]))
+        self.assertFalse(jrtimeman.credentials.has_env_vars(["JAMIE", "COLIN"]))
 
 
 if __name__ == "__main__":
